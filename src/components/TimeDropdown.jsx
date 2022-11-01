0d7a535a6bdd3aa7277155d2e2c11d827ef57b77
vivek reddy <vivekreddy74.n@gmail.com>
2022-11-01 15:54:53 +0530

    minor change

--- src/components/TimeDropdown.jsx
+++ src/components/TimeDropdown.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import { useSearchParams, useNavigate, useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 function TimeDropDown (props) {
   const {isLoading} = props
-  const navigate = useNavigate()
-  const location = useLocation()
-  const [searchParams] = useSearchParams();
-  const path = location.pathname.split('?')[0]
+  const [searchParams, setSearchParams] = useSearchParams();
   function handleChange (event) {
-    navigate(`${path}?time=${event.target.value}`)
+
+    searchParams.set('time', event.target.value)
+    setSearchParams(searchParams)
   }
 
   return (
@@ -20,7 +19,6 @@ function TimeDropDown (props) {
           inputProps={{ 'aria-label': 'Without label' }}
           disabled={isLoading}
         >
-          
           <MenuItem value='all-time'>All Time</MenuItem>
           <MenuItem value='6-months'>Past 6 months</MenuItem>
           <MenuItem value='5-years'>Past 5 years</MenuItem>
